45684abbb45dff94e86f06e6eb9d7d8b15abd84b
Henry Cui <z.henry.cui@gmail.com>
2018-05-14 15:12:38 +0900

    Enhanced Figure 1 script

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 This is the **Chainer** implementation for the paper [The Lottery Ticket Hypothesis: Training Pruned Neural Network Architectures](https://arxiv.org/abs/1803.03635).
 
 #### Figure 1
-`python figure1.py --hidden 2 4 6 8 10 --iter 1000 --epoch 1000`
+`python figure1.py --hidden 2 4 6 8 10 --iter 1000 --epoch 10000 --minloss 1e-7 --patient 7`
 
 - hidden: Number list for the hidden layer.
 - iter: Number of iterations for each number of hidden layer.
 - epoch: Number of epochs for one iteration.
+- minloss: Minimal change of loss for early stopping.
+- patient: Counts to stop.
--- figure1.py
+++ figure1.py
@@ -5,11 +5,11 @@ import chainer
 import chainer.functions as F
 import chainer.links as L
 from chainer.datasets import TupleDataset
-from chainer.training import StandardUpdater, Trainer
 import numpy as np
 
+
 class MLP(chainer.Chain):
-    def __init__(self, H):
+    def __init__(self, H, i, I, db, zl):
         super(MLP, self).__init__()
         with self.init_scope():
             self.l1 = L.Linear(2, H)
@@ -27,61 +27,84 @@ class MLP(chainer.Chain):
         self.y = self.xp.array(
             [-1, -1, 1, 1], dtype=self.xp.int32)[:, None]
 
-    def __call__(self, x, y):
-        res = self.forward(x)
-        loss = F.sigmoid_cross_entropy(res, y)
+    def loss(self, x, y):
+        loss = F.sigmoid_cross_entropy(self.res_y(), y)
         return loss
 
     def if_db(self):
         return not ((self.y * self.res_y()).data < 0).any()
 
-    def if_zl(self):
-        return F.sigmoid_cross_entropy(self.res_y(), self.y).data[()] == 0.
-
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hidden', nargs='+', type=int, default=[2])
+    parser.add_argument('--hidden', nargs='+',
+                        type=int, default=[2, 4, 6, 8, 10])
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--iter', type=int, default=1000)
-    parser.add_argument('--epoch', type=int, default=1000)
+    parser.add_argument('--epoch', type=int, default=10000)
+    parser.add_argument('--minloss', type=float, default=1e-7)
+    parser.add_argument('--patient', type=int, default=7)
     args = parser.parse_args()
 
     for h in args.hidden:
         db = 0
         zl = 0
+        sl = 0
         for i in range(args.iter):
-            model = MLP(h)
+            model = MLP(h, i, args.iter, db, zl)
 
             if args.gpu >= 0:
                 chainer.backends.cuda.get_device_from_id(args.gpu).use()
                 model.to_gpu()
 
-            model.set_xy()
-            optimizer = chainer.optimizers.SGD()
+            optimizer = chainer.optimizers.SGD(1.)
             optimizer.setup(model)
-            train = TupleDataset(
-                model.xp.array(
-                    [[0, 0], [1, 1], [0, 1], [1, 0]], dtype=model.xp.float32),
-                model.xp.array([0, 0, 1, 1], dtype=model.xp.int32)[:, None])
-            train_iter = chainer.iterators.SerialIterator(train, batch_size=4)
 
-            updater = StandardUpdater(train_iter, optimizer, device=args.gpu)
-            trainer = Trainer(updater, (args.epoch, 'epoch'))
-            trainer.run()
+            train = model.xp.array([0, 0, 1, 1],
+                                   dtype=model.xp.int32)[:, None]
+
+            epoch = 0
+            loss_diff = 100
+            old_loss = None
+            patient = 1
+            model.set_xy()
+            for epoch in range(args.epoch):
+                loss = model.loss(model.x, train)
+
+                model.cleargrads()
+                loss.backward()
+                optimizer.update()
+                print(' ' * 100, end='\r')
+                print('H = {} ITER {} / {} iter, DB {:.3f},'\
+                      ' ZL {:.3f}, SL {:.3f}, LOSS {}, {}'.format(
+                          h, i, args.iter, db / args.iter, zl / args.iter,
+                          sl / args.iter, loss.data, epoch), end='\r')
+
+                loss.to_cpu()
+                if old_loss is None:
+                    old_loss = loss.data
+                else:
+                    loss_diff = np.abs(old_loss - loss.data)
+                    old_loss = loss.data
+
+                if loss_diff < args.minloss:
+                    if patient > args.patient:
+                        break
+                    else:
+                        patient += 1
+                else:
+                    patient = 0
 
             if model.if_db():
                 db += 1
-            if model.if_zl():
+            if loss.data == 0.:
                 zl += 1
+            if loss.data < args.minloss:
+                sl += 1
 
-            print('', end='\r')
-            print('H = {}: {}/{} iter, DB {:.3f}, ZL {:.3f}'.format(
-                h, i, args.iter, db / (i + 1), zl / (i + 1)), end='\r')
-
-        print('', end='\r')
-        print('{} hidden: {} iter, DB {:.3f}, ZL {:.3f}'.format(
-            h, args.iter, db / args.iter, zl / args.iter))
+        print(' ' * 100, end='\r')
+        print('H {}: {} iter, DB {:.3f}, ZL {:.3f}, SL {:.3f}'.format(
+            h, args.iter, db / args.iter, zl / args.iter, sl / args.iter))
 
 
 if __name__ == '__main__':
